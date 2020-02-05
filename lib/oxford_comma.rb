73c865fb531766f8b098bef5874584a9754bd952
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-05 02:37:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,14 @@
 def oxford_comma(array)
+  num = array.count
+  if num > 2
+  and_last = "and " + array.pop
+  array.push(and_last)  
+  array.join(", ")
+ elsif num <= 2
   and_last = "and " + array.pop
   array.push(and_last)
-  array.join(", ")
-
+  array.join(" ")
+else 
+  array.join("")
+end    
 end
